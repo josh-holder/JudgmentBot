7b704e02ef740b5f491320d93f5d1f9e9a063029
Josh Holder <jgh8@rice.edu>
2022-02-27 14:06:15 -0600

    housekeeping

--- agent_compare.py
+++ agent_compare.py
@@ -1,5 +1,6 @@
 from audioop import avg
 from JudgmentGame import JudgmentGame
+from NNAgent import NNAgent
 from SimpleAgent import SimpleAgent
 from JudgmentAgent import JudgmentAgent
 import random
@@ -10,11 +11,14 @@ def compareAgents(agents_to_compare,games_num=1000):
     for game_num in range(games_num):
         print("Simulating game {}/{}".format(game_num,games_num),end='\r')
         starting_agent = random.choice(range(len(agents_to_compare)))
-        random_agent_order = deepcopy(agents_to_compare[starting_agent:]+agents_to_compare[:starting_agent])
+
+        random_agent_order = agents_to_compare[starting_agent:]+agents_to_compare[:starting_agent]
+
         jg = JudgmentGame(agents=random_agent_order)
         resulting_agents = jg.playGame()
         for i,agent in enumerate(resulting_agents):
             scores[i] += agent.points
+            agent.points = 0 #reset points
 
     avg_scores = [score/games_num for score in scores]
     print("Average Final Scores over {} games: {}".format(games_num,avg_scores))
@@ -22,4 +26,4 @@ def compareAgents(agents_to_compare,games_num=1000):
     return avg_scores
 
 if __name__ == "__main__":
-    compareAgents([JudgmentAgent(0),SimpleAgent(1),JudgmentAgent(2),JudgmentAgent(3)])
+    compareAgents([NNAgent(0),NNAgent(1),SimpleAgent(2),SimpleAgent(3)],games_num=1000)
