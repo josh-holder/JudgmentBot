3bc24e197753145638bcda1120da040ef889b394
Josh Holder <jgh8@rice.edu>
2022-12-23 23:37:44 -0500

    loading exp data

--- dqn_train.py
+++ dqn_train.py
@@ -1,15 +1,62 @@
 from collections import deque
 import pickle
 import os
+from nn_config import ACTION_EXPERIENCE_BANK_SIZE, BET_EXPERIENCE_BANK_SIZE, EVAL_EXPERIENCE_BANK_SIZE
+import argparse
+
+def _build_parser():
+    parser = argparse.ArgumentParser(description='Run NN to generate ideal playing strategy for SPLT.')
+
+    parser.add_argument(
+        '-r','--run_name',
+        help="Will name the output directory for the results of the run",
+        type=str,
+        default="run",
+    )
+
+    return parser
+
+def loadExperienceData(run_name):
+    """
+    Loads exists, or creates new experience data to use for experience replay.
+    """
+    folder_name = "dqn_experience_data"
+
+    bet_mem_path = os.path.join(os.getcwd(),folder_name,run_name,"bet_experience_data.pkl")
+    if os.path.exists(bet_mem_path):
+        print("Loading existing bet experience data.")
+        with open(bet_mem_path,'rb') as f:
+            bet_experience_data = pickle.load(f)
+    else:
+        print("Previous bet experience data not found: generating empty memory list.")
+        bet_experience_data = deque(maxlen=BET_EXPERIENCE_BANK_SIZE)
+
+    eval_mem_path = os.path.join(os.getcwd(),folder_name,run_name,"eval_experience_data.pkl")
+    if os.path.exists(eval_mem_path):
+        print("Loading existing eval experience data.")
+        with open(eval_mem_path,'rb') as f:
+            eval_experience_data = pickle.load(f)
+    else:
+        print("Previous eval experience data not found: generating empty memory list.")
+        eval_experience_data = deque(maxlen=EVAL_EXPERIENCE_BANK_SIZE)
+    
+    act_mem_path = os.path.join(os.getcwd(),folder_name,run_name,"act_experience_data.pkl")
+    if os.path.exists(act_mem_path):
+        print("Loading existing action experience data.")
+        with open(act_mem_path,'rb') as f:
+            act_experience_data = pickle.load(f)
+    else:
+        print("Previous action experience data not found: generating empty memory list.")
+        act_experience_data = deque(maxlen=ACTION_EXPERIENCE_BANK_SIZE)
+
+    return bet_experience_data, eval_experience_data, act_experience_data
 
 def trainDQNAgent():
-    print("hi")
-    folder_name = "DQNRuns"
-    act_mem_path = os.path.join(os.getcwd(),folder_name,"game_memory.pkl")
-	if os.path.exists(mem_path):
-		print("Loading existing experience data.")
-		with open(mem_path,'rb') as f:
-			memory = pickle.load(f)
-	else:
-		print("Previous experience data not found: generating empty memory list.")
-		memory = deque(maxlen=nn_config.MEMORY_SIZE)
+    parser = _build_parser()
+    args = parser.parse_args()
+
+    bet_exp_data, eval_exp_data, act_exp_data = loadExperienceData(args.run_name)
+
+
+if __name__ == "__main__":
+    trainDQNAgent()    
