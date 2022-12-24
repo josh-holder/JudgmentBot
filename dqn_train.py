46c1a1c8e50a3e071577c41d734178c2f7a57fdb
Josh Holder <jgh8@rice.edu>
2022-12-24 00:22:11 -0500

    some thoughts on next steps for dqn_train

--- dqn_train.py
+++ dqn_train.py
@@ -4,6 +4,9 @@ import os
 from nn_config import ACTION_EXPERIENCE_BANK_SIZE, BET_EXPERIENCE_BANK_SIZE, EVAL_EXPERIENCE_BANK_SIZE
 import argparse
 
+from JudgmentGame import JudgmentGame
+from DQNAgent import DQNAgent
+
 def _build_parser():
     parser = argparse.ArgumentParser(description='Run NN to generate ideal playing strategy for SPLT.')
 
@@ -51,11 +54,33 @@ def loadExperienceData(run_name):
 
     return bet_experience_data, eval_experience_data, act_experience_data
 
+def addToExperienceBank(exp_data, game_data):
+    """
+    Add data from current game to it's respective experience bank.
+
+    TODO
+    """
+
+    pass
+
+
 def trainDQNAgent():
     parser = _build_parser()
     args = parser.parse_args()
 
     bet_exp_data, eval_exp_data, act_exp_data = loadExperienceData(args.run_name)
+
+    jg = JudgmentGame(agents=[DQNAgent(0),DQNAgent(1),DQNAgent(2),DQNAgent(3)])
+    #Wait until all experience banks are at least 1/4 full to start learning:
+    while len(bet_exp_data)<BET_EXPERIENCE_BANK_SIZE/4 or len(eval_exp_data)<EVAL_EXPERIENCE_BANK_SIZE/4 or len(act_exp_data)<ACTION_EXPERIENCE_BANK_SIZE/4:
+        bet_game_data, eval_game_data, action_game_data = jg.playGameAndTrackData()
+        #Need to be tracking next state, or reward if it is terminal.
+        #playGameAndTrackData is actually collecting data we need for to train the value functions on
+        #undiscounted monte carlo rewards, rather than actual one-step rewards.
+
+        #tuple should now be (input state, input action, reward, next state)
+        #Then when training on the reward later, recalculate expected value of next state, add to reward
+        #How to implement gradient descent a la equation 3? Look at implementations in the literature.
 
 
 if __name__ == "__main__":
