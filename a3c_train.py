c49c6340a3f596e7acbd3489c022d51dfc0a8ec6
Josh Holder <josh.holder72@gmail.com>
2023-03-17 16:41:01 -0700

    starting to flesh this out

--- a3c_train.py
+++ a3c_train.py
@@ -1,8 +1,7 @@
 from collections import deque
 import pickle
 import os
-from nn_config import ACTION_EXPERIENCE_BANK_SIZE, BET_EXPERIENCE_BANK_SIZE, EVAL_EXPERIENCE_BANK_SIZE, \
-    RETRAIN_BATCH_SIZE, RETRAIN_EPOCHS
+import nn_config
 import argparse
 import numpy as np
 import random
@@ -17,6 +16,9 @@ from DQNAgent import DQNAgent
 from compare_agents import compareAgents
 from HumanBetAgent import HumanBetAgent
 from multiprocessing import cpu_count, Queue
+import wandb
+from copy import copy, deepcopy
+from multiprocessing import Process, cpu_count, Queue
 
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
 
@@ -27,7 +29,7 @@ def _build_parser():
         '-r','--run_name',
         help="Will name the output directory for the results of the run",
         type=str,
-        default="run1",
+        default="test_run",
     )
 
     parser.add_argument(
@@ -51,7 +53,56 @@ def _build_parser():
         default="action_expert_train_model",
     )
 
+    parser.add_argument(
+        '-m','--models_path',
+        help="Path to folder containing models, if you're restarting from a previous run. Defaults to None, in which case will use -b, -e, and -a to find models.",
+        default=None,
+    )
+
+    parser.add_argument(
+        '-t','--track',
+        help="Flag determining whether to track this run in weights and biases",
+        action="store_true",
+        default=False,
+    )
+
+    parser.add_argument(
+        '--wandb_project_name',
+        help="Name of WandB project to log to. Only relevant if args.track=True.",
+        type=str,
+        default="JudgementBot",
+    )
+
+    parser.add_argument(
+        "--wandb-entity", 
+        type=str, 
+        default="josh-holder", 
+        help="the entity (team) of wandb's project"
+    )
+
     return parser
+
+def initWandBTrack(args):
+    #initialize weights and biases tracking
+    run_name = f"{args.run_name}__{int(time.time())}"
+    config_dict = vars(args)
+    config_dict.pop("track",None)
+    config_dict.pop("wandb_project_name",None)
+    config_dict.pop("wandb_entity",None)
+    config_dict["algo"] = "a3c"
+    
+    nn_config_dict = nn_config.__dict__
+    for key in list(nn_config.__dict__.keys()):
+        if key.startswith("_"): nn_config_dict.pop(key,None)
+
+    config_dict = {**config_dict, **nn_config.__dict__}
+
+    wandb.init(
+        name=run_name,
+        entity=args.wandb_entity,
+        project=args.wandb_project_name,
+        config=config_dict,
+    )
 
 def loadExperienceData(run_name, folder_name='dqn_experience_data'):
     """
@@ -72,7 +123,7 @@ def loadExperienceData(run_name, folder_name='dqn_experience_data'):
         print(f"Done loading {len(act_experience_data)} items of action experience data")
     else:
         print("Previous action experience data not found: generating empty memory list.")
-        act_experience_data = deque(maxlen=ACTION_EXPERIENCE_BANK_SIZE)
+        act_experience_data = deque(maxlen=nn_config.ACTION_EXPERIENCE_BANK_SIZE)
 
     bet_mem_path = os.path.join(os.getcwd(),run_name,folder_name,"bet_experience_data.pkl")
     if os.path.exists(bet_mem_path):
@@ -82,7 +133,7 @@ def loadExperienceData(run_name, folder_name='dqn_experience_data'):
         print(f"Done loading {len(bet_experience_data)} items of bet experience data")
     else:
         print("Previous bet experience data not found: generating empty memory list.")
-        bet_experience_data = deque(maxlen=BET_EXPERIENCE_BANK_SIZE)
+        bet_experience_data = deque(maxlen=nn_config.BET_EXPERIENCE_BANK_SIZE)
 
     eval_mem_path = os.path.join(os.getcwd(),run_name,folder_name,"eval_experience_data.pkl")
     if os.path.exists(eval_mem_path):
@@ -92,7 +143,7 @@ def loadExperienceData(run_name, folder_name='dqn_experience_data'):
         print(f"Done loading {len(eval_experience_data)} items of eval experience data")
     else:
         print("Previous eval experience data not found: generating empty memory list.")
-        eval_experience_data = deque(maxlen=EVAL_EXPERIENCE_BANK_SIZE)
+        eval_experience_data = deque(maxlen=nn_config.EVAL_EXPERIENCE_BANK_SIZE)
 
     return bet_experience_data, eval_experience_data, act_experience_data
 
@@ -115,6 +166,56 @@ def saveExperienceData(run_name,bet_exp_data,eval_exp_data,state_transition_bank
     with open(act_mem_path,'wb') as f:
         print(f"Saving {len(state_transition_bank)} items of state transition data in {act_mem_path}")
         pickle.dump(state_transition_bank,f)
+
+def loadModels(args):
+    """
+    Given command line arguments, loads current, baseline, and target models.
+    """
+    if args.models_path == None:
+        curr_bet_model_path = os.path.join(os.getcwd(),args.bet_model_path)
+        curr_bet_model = keras.models.load_model(curr_bet_model_path)
+        print(f"Loaded current bet model from {curr_bet_model_path}")
+
+        curr_eval_model_path = os.path.join(os.getcwd(),args.eval_model_path)
+        curr_eval_model = keras.models.load_model(curr_eval_model_path)
+        print(f"Loaded current eval model from {curr_eval_model_path}")
+
+        curr_action_model_path = os.path.join(os.getcwd(),args.action_model_path)
+        curr_action_model = keras.models.load_model(curr_action_model_path)
+        print(f"Loaded current action model fron {curr_action_model_path}")
+    
+    else:
+        curr_bet_model_path = os.path.join(os.getcwd(),args.models_path,"best_bet_model")
+        curr_bet_model = keras.models.load_model(curr_bet_model_path)
+        print(f"Loaded current bet model from {curr_bet_model_path}")
+
+        curr_eval_model_path = os.path.join(os.getcwd(),args.models_path,"best_eval_model")
+        curr_eval_model = keras.models.load_model(curr_eval_model_path)
+        print(f"Loaded current eval model from {curr_eval_model_path}")
+
+        curr_action_model_path = os.path.join(os.getcwd(),args.models_path,"best_act_model")
+        curr_action_model = keras.models.load_model(curr_action_model_path)
+        print(f"Loaded current action model fron {curr_action_model_path}")
+
+    baseline_bet_model = tf.keras.models.clone_model(curr_bet_model)
+    baseline_bet_model.set_weights(curr_bet_model.get_weights())
+
+    baseline_eval_model = tf.keras.models.clone_model(curr_eval_model)
+    baseline_eval_model.set_weights(curr_eval_model.get_weights())
+
+    baseline_action_model = tf.keras.models.clone_model(curr_action_model)
+    baseline_action_model.set_weights(curr_action_model.get_weights())
+
+    for layer in baseline_bet_model.layers:
+        layer.trainable = False
+    for layer in baseline_eval_model.layers:
+        layer.trainable = False
+    for layer in baseline_action_model.layers:
+        layer.trainable = False
+
+    print("Initialized target models as untrainable copies of current models.")
+
+    return curr_bet_model, baseline_bet_model, curr_eval_model, baseline_eval_model, curr_action_model, baseline_action_model
 
 def saveModels(run_name, bet_model, eval_model, act_model):
     bet_model_path = os.path.join(os.getcwd(),run_name,"best_bet_model")
@@ -157,7 +258,6 @@ def convertMiniBatchToStateRewardPair(state_transition_minibatch, action_model, 
         converted_minibatch.append((action_state, reward))
 
     return converted_minibatch
-            
 
 def trainAgentViaA3C():
     parser = _build_parser()
@@ -167,30 +267,12 @@ def trainAgentViaA3C():
     if not os.path.exists(run_folder_path):
         os.mkdir(run_folder_path)
 
-    print("Loading models...")
-    bet_model_path = os.path.join(os.getcwd(),args.bet_model_path)
-    bet_model = keras.models.load_model(bet_model_path)
-    print(f"Loaded bet model from {bet_model_path}")
+    curr_bet_model, baseline_bet_model, curr_eval_model, baseline_eval_model, curr_action_model, baseline_action_model = loadModels(args)
 
-    eval_model_path = os.path.join(os.getcwd(),args.eval_model_path)
-    eval_model = keras.models.load_model(eval_model_path)
-    print(f"Loaded eval model from {eval_model_path}")
-
-    action_model_path = os.path.join(os.getcwd(),args.action_model_path)
-    action_model = keras.models.load_model(action_model_path)
-    print(f"Loaded action model fron {action_model_path}")
-
-    for agent in jg.agents:
-        agent.action_model = action_model
-        agent.bet_model = bet_model
-        agent.eval_model = eval_model
-
-    performance_against_humanbet = []
-    performance_against_prev_agents = []
+    performance_against_best_agents = []
     new_states_to_achieve_performances = []
 
     new_state_action_pairs_trained_on = 0
-    beat_humanbet_by = 0 #track how much the previous agent beat the HumanBetAgent by to determine if the nnew agent is better
 
     """
     Psuedocode:
@@ -207,12 +289,23 @@ def trainAgentViaA3C():
     while True:
         processes = []
 
-        act_gradients = Queue()
-        act_gradients.put([zero gradients])
+        accum_act_gradients_queue = Queue()
+        accum_act_gradients = [tf.zeros_like(var) for var in curr_action_model.trainable_variables]
+        accum_act_gradients_queue.put(accum_act_gradients)
+
+        accum_bet_gradients_queue = Queue()
+        accum_bet_gradients = [tf.zeros_like(var) for var in curr_bet_model.trainable_variables]
+        accum_bet_gradients_queue.put(accum_bet_gradients)
+
+        accum_eval_gradients_queue = Queue()
+        accum_eval_gradients = [tf.zeros_like(var) for var in curr_eval_model.trainable_variables]
+        accum_eval_gradients_queue.put(accum_eval_gradients)
+
         #bet and eval gradients
         for core_num in range(cpu_count()):
             epsilon_choice = random.choice(epsilon_choices)
-            p = Process(target=playJudgmentGameThread(action_model, bet_model, eval_model, epsilon_choice)) 
+            p = Process(target=playJudgmentGameThread(curr_action_model, curr_bet_model, curr_eval_model, epsilon_choice, \
+                                                      accum_act_gradients_queue, accum_bet_gradients_queue, accum_eval_gradients_queue)) 
 
             processes.append(p)
             p.start()
@@ -226,56 +319,72 @@ def trainAgentViaA3C():
         #~~~~~~~~~~~~~~~~~~~~~~EVALUATING PERFORMANCE~~~~~~~~~~~~~~~~~~~~~~~
         print("Bet and Evaluation models retrained on new data: evaluating performance.")
 
-        #epsilon is zero for evaluation. Load new models into agents 0 and 1, old models into agents 2 and 3.
+        #epsilon is zero for evaluation. Load new models into agents 0 and 1, best models into agents 2 and 3.
         agents_to_compare = [DQNAgent(0,load_models=False),DQNAgent(1,load_models=False),DQNAgent(2,load_models=False),DQNAgent(3,load_models=False)]
         for agent in agents_to_compare[0:2]:
-            agent.action_model = action_model
-            agent.bet_model = bet_model
-            agent.eval_model = eval_model
+            agent.action_model = curr_action_model
+            agent.bet_model = curr_bet_model
+            agent.eval_model = curr_eval_model
 
         for agent in agents_to_compare[2:]:
-            agent.action_model = old_action_model
-            agent.bet_model = old_bet_model
-            agent.eval_model = old_eval_model
+            agent.action_model = baseline_action_model
+            agent.bet_model = baseline_bet_model
+            agent.eval_model = baseline_eval_model
 
-        print("Performance against previous agent iteration:")
-        avg_scores_against_prev_agents = compareAgents(agents_to_compare,games_num=20, cores=cpu_count())
-        new_agent_score_against_prev = sum(avg_scores_against_prev_agents[0:2])/len(avg_scores_against_prev_agents[0:2])
-        prev_agent_score_against_new = sum(avg_scores_against_prev_agents[2:])/len(avg_scores_against_prev_agents[2:])
-        print(f"New Agent average score: {new_agent_score_against_prev}, previous agent average score: {prev_agent_score_against_new}")
+        print("Performance against baseline agent:")
+        avg_scores_against_baseline_agents = compareAgents(agents_to_compare,games_num=24, cores=cpu_count())
+        new_agent_score_against_baseline = sum(avg_scores_against_baseline_agents[0:2])/len(avg_scores_against_baseline_agents[0:2])
+        baseline_agent_score_against_new = sum(avg_scores_against_baseline_agents[2:])/len(avg_scores_against_baseline_agents[2:])
+        print(f"New Agent average score: {new_agent_score_against_baseline}, baseline agent average score: {baseline_agent_score_against_new}")
 
-        print("Performance against HumanBetAgents:")
-        avg_scores_against_humanbet_agents = compareAgents([agents_to_compare[0],agents_to_compare[1],HumanBetAgent(2),HumanBetAgent(3)],games_num=20, cores=cpu_count())
-        new_agent_score_against_humanbet = sum(avg_scores_against_humanbet_agents[0:2])/len(avg_scores_against_humanbet_agents[0:2])
-        humanbet_agent_score_against_new = sum(avg_scores_against_humanbet_agents[2:])/len(avg_scores_against_humanbet_agents[2:])
-        print(f"New Agent average score: {new_agent_score_against_humanbet}, HumanBet agent average score: {humanbet_agent_score_against_new}")
+        current_beat_baseline_by = new_agent_score_against_baseline - baseline_agent_score_against_new
 
-        if (new_agent_score_against_prev > prev_agent_score_against_new) or (new_agent_score_against_humanbet > beat_humanbet_by):
-            print("New agent improves on old agent, so saving it.")
-            saveModels(args.run_name, bet_model, eval_model, action_model)
-            beat_humanbet_by = new_agent_score_against_humanbet - humanbet_agent_score_against_new
+        # print("Performance against HumanBetAgents:")
+        # avg_scores_against_humanbet_agents = compareAgents([agents_to_compare[0],agents_to_compare[1],HumanBetAgent(2),HumanBetAgent(3)],games_num=20, cores=cpu_count())
+        # new_agent_score_against_humanbet = sum(avg_scores_against_humanbet_agents[0:2])/len(avg_scores_against_humanbet_agents[0:2])
+        # humanbet_agent_score_against_new = sum(avg_scores_against_humanbet_agents[2:])/len(avg_scores_against_humanbet_agents[2:])
+        # print(f"New Agent average score: {new_agent_score_against_humanbet}, HumanBet agent average score: {humanbet_agent_score_against_new}")
+
+        if (current_beat_baseline_by > best_model_beat_baseline_by):
+            print(f"!!!New agent improves on current best agent (beating baseline by {current_beat_baseline_by} instead of {best_model_beat_baseline_by}), so saving it.!!!")
+            best_bet_weights = copy(curr_bet_model.get_weights())
+            best_action_weights = copy(curr_action_model.get_weights())
+            best_eval_weights = copy(curr_eval_model.get_weights())
+
+            saveModels(args.run_name, curr_bet_model, curr_eval_model, curr_action_model)
+
+            iterations_without_improving_best_agent = 0
+            best_model_beat_baseline_by = current_beat_baseline_by
         else:
-            print("New agent does not improve on old agent, so does not save the model.")
-            bet_model = old_bet_model
-            eval_model = old_eval_model
-            action_model = old_action_model
+            iterations_without_improving_best_agent += 1
+            
+            if iterations_without_improving_best_agent >= 3:
+                print(f"It has been {iterations_without_improving_best_agent} iterations without improving on best agent, so reset to old best agent.")
+                #remove the last ~50 games of data from the buffer
+                action_rb_data = [action_rb_data.pop() for _i in range(3*nn_config.NUM_NEW_TRANSITIONS_BEFORE_EVAL_BET_TRAIN)]
+                eval_rb_data = [eval_rb_data.pop() for _i in range(3*nn_config.NUM_NEW_TRANSITIONS_BEFORE_EVAL_BET_TRAIN//3)] 
+                bet_rb_data = [bet_rb_data.pop() for _i in range(3*nn_config.NUM_NEW_TRANSITIONS_BEFORE_EVAL_BET_TRAIN//6)]
+
+                curr_bet_model.set_weights(best_bet_weights)
+                curr_action_model.set_weights(best_action_weights)
+                curr_eval_model.set_weights(best_eval_weights)
+
+                iterations_without_improving_best_agent = 0
+            else: print(f"~~~New agent does not improve on best agent (beat baseline by {current_beat_baseline_by} instead of {best_model_beat_baseline_by}), so increase iterations without improving on best to {iterations_without_improving_best_agent} and continue training.~~~")
 
         print(f"Evaluation complete: generating {num_new_transitions_before_eval_bet_training} new state transitions.")
 
+        if args.track: wandb.log({"eval/score_diff_against_baseline": current_beat_baseline_by, "eval/state_transitions": new_state_action_pairs_trained_on})
+
         #save data for progress plots
-        performance_against_humanbet.append(new_agent_score_against_humanbet-humanbet_agent_score_against_new)
         new_states_to_achieve_performances.append(new_state_action_pairs_trained_on)
-        plt.plot(new_states_to_achieve_performances,performance_against_humanbet, label='vs. HumanBet')
+        performance_against_best_agents.append(current_beat_baseline_by)
+        plt.plot(new_states_to_achieve_performances,performance_against_best_agents)
         plt.xlabel("Number of new training examples")
-        plt.ylabel("Avg. Score Difference vs. HumanBet")
+        baseline_model_name = args.action_model_path.split("/")[0]
+        plt.ylabel(f"Avg. Score Diff vs. Baseline Model ({baseline_model_name})")
 
-        performance_against_prev_agents.append(new_agent_score_against_prev-prev_agent_score_against_new)
-        plt.plot(new_states_to_achieve_performances,performance_against_prev_agents, label='vs. previous iteration')
-        plt.xlabel("Number of new training examples")
-        plt.ylabel("Avg. Score Diff vs. Prev. Iteration")
-
-        progress_graph_path = os.path.join(os.getcwd(), args.run_name, "performance_vs_prev_agent_over_time.png")
-        plt.legend()
+        progress_graph_path = os.path.join(os.getcwd(), args.run_name, "performance_vs_baseline_agent_over_time.png")
         plt.savefig(progress_graph_path)
         plt.clf()
 
@@ -283,7 +392,7 @@ if __name__ == "__main__":
     # jg = JudgmentGame(agents=[DQNAgent(0),DQNAgent(1),DQNAgent(2),DQNAgent(3)])
     # bet_exp_data, eval_exp_data, state_transition_bank = loadExperienceData("run1")
 
-    trainDQNAgent()
+    trainAgentViaA3C()
 
     # bet_data, eval_data, state_transitions = jg.playGameAndTrackStateTransitions()
 
