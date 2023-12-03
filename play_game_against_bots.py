2a8e6852274aa8a0612dbc96c624357842c1e994
Josh Holder <jgh8@rice.edu>
2023-12-03 11:12:47 -0800

    updating play against bots

--- play_game_against_bots.py
+++ play_game_against_bots.py
@@ -7,7 +7,7 @@ from NNAgent import NNAgent
 play_verbose = 0
 
 if __name__ == "__main__":
-    jg = JudgmentGame(agents=[HumanAgent(0),NNAgent(1,bet_model_name="run1/best_bet_model",action_model_name="run1/best_act_model",eval_model_name="run1/best_eval_model"),\
-            NNAgent(2,bet_model_name="run1/best_bet_model",action_model_name="run1/best_act_model",eval_model_name="run1/best_eval_model"),\
-            NNAgent(3,bet_model_name="run1/best_bet_model",action_model_name="run1/best_act_model",eval_model_name="run1/best_eval_model")],print_final_tables=1)
+    jg = JudgmentGame(agents=[HumanAgent(0),NNAgent(1,bet_model_name="current_best_models/best_bet_model",action_model_name="current_best_models/best_act_model",eval_model_name="current_best_models/best_eval_model"),\
+            NNAgent(2,bet_model_name="current_best_models/best_bet_model",action_model_name="current_best_models/best_act_model",eval_model_name="current_best_models/best_eval_model"),\
+            NNAgent(3,bet_model_name="current_best_models/best_bet_model",action_model_name="current_best_models/best_act_model",eval_model_name="current_best_models/best_eval_model")],print_final_tables=1)
     jg.playGame()
